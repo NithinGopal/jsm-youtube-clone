6eb4601b4fdd9cfc4d6f7a8e1f563fb81354323d
Nithin Gopal <nithingopal2015@gmail.com>
2022-08-30 17:35:17 +0530

    add redirects file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Stack } from '@mui/material'
+import { Stack, Typography } from '@mui/material'
 import { Link } from 'react-router-dom'
 import { logo } from '../utils/constants'
 import SearchBar from './SearchBar'
@@ -17,8 +17,9 @@ const Navbar = () => {
         justifyContent: 'space-between'
       }}
     >
-      <Link to='/' style={{ display: 'flex', alignItems: 'center' }}>
+      <Link to='/' style={{ display: 'flex', alignItems: 'center', gap:'1rem' }}>
         <img src={logo} alt="logo" height={45} />
+        <Typography variant='h4' fontWeight='bold' color='#008245' >ReactTube</Typography>
       </Link>
       <SearchBar />
     </Stack>
